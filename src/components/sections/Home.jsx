7d454a2ca44a5133ef83df2fe1b8507333256ea5
Amitesh218 <amiteshrawal1@gmail.com>
2025-03-07 12:15:48 +0530

    this better work bruh

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-pink-500 to-red-400 bg-clip-text text-transparent leading-right">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-green-500 to-red-400 bg-clip-text text-transparent leading-right">
             Hi, I'm Amitesh
           </h1>
 
@@ -27,8 +27,8 @@ export const Home = () => {
 
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
-             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 bg-black
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-900/90"
             >
               Contact Me
             </a>
